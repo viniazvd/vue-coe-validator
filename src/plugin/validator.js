f9a6671372ba0755310d84ef7a86331a122f3cab
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-09-03 16:29:11 -0300

    refactor #1

--- src/plugin/validator.js
+++ src/plugin/validator.js
@@ -7,17 +7,18 @@ function isValid (pattern, required, value) {
 }
 
 export const validator = function (Vue, options) {
-  Vue.prototype.$init = function (form, name) {
+  Vue.prototype.$init = function (form, name, rules) {
     const defaultState = {
-      $isTouched: false,
-      $isDirty: false,
-      $isFilled: false,
-      $isValid: false
+      isTouched: false,
+      isDirty: false,
+      isFilled: false,
+      isValid: false
     } 
     
     const newForm = {
       [name]: Object.entries(form).reduce((form, [key, value]) => {
-        form[key] = { key, value, ...defaultState }
+        const isFilled = { $isFilled: !!value }
+        form[key] = { key, value, ...defaultState, ...isFilled, ...rules[key] }
 
         return form
       }, {})
@@ -27,21 +28,19 @@ export const validator = function (Vue, options) {
     this.initialForm = { ...this.initialForm, ...newForm }
   }
 
-  Vue.prototype.$synchronize = function ({ 
-    form = '',
-    key, 
-    value, 
-    required = false,
-    pattern = '' 
-  }) {
+  Vue.prototype.$synchronize = function (value, key, form = '') {
     if (!form) { console.warn('select a form to synchronize the data.') }
+
+    const pattern = Object.keys(this.forms).length > 1 ? this.forms[form][key].pattern : this.forms[0][key].pattern
+    const required = Object.keys(this.forms).length > 1 ? this.forms[form][key].required : this.forms[0][key].required
+
     const changed = {
       ...this.forms[form][key],
       value,
-      $isTouched: true,
-      $isDirty: true,
-      $isFilled: !!value,
-      $isValid: isValid(pattern, required, value)
+      isTouched: true,
+      isDirty: true,
+      isFilled: !!value,
+      isValid: isValid(pattern, required, value)
     }
     
     const inputUpdated = { [key]: changed }
