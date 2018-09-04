da3022683b0215c059b9315cbaf4074a1453dc05
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-03 23:24:14 -0300

    update example

--- src/Root.vue
+++ src/Root.vue
@@ -6,17 +6,17 @@
         <c-input 
           label="input1"
           :validation="$hasError('input1')"
-          :value="$getValue('input1', 'form1')"
-          @input="value => $synchronize(value, 'input1', 'form1')"
-          @blur="$touch('input1', 'form1')"
+          :value="$getValue('input1')"
+          @input="value => $synchronize(value, 'input1')"
+          @blur="$touch('input1')"
         />
         
         <c-input 
           label="input2" 
-          :validation="$hasError('input2', 'form1')"
-          :value="$getValue('input2', 'form1')"
-          @input="value => $synchronize(value, 'input2', 'form1')"
-          @blur="$touch('input2', 'form1')"
+          :validation="$hasError('input2')"
+          :value="$getValue('input2')"
+          @input="value => $synchronize(value, 'input2')"
+          @blur="$touch('input2')"
         />
       </form>
 
@@ -87,9 +87,6 @@ export default {
   components: { CInput },
 
   methods: {
-    touch () {
-      console.log('das')
-    },
     submit1 (form) {
       this.$allTouched(form)
       const isValid = this.$isValidForm(form)
@@ -109,8 +106,8 @@ export default {
   },
 
   mounted () {
-    this.$init(form1, 'form1', customRules1)
-    this.$init(form2, 'form2', customRules2)
+    this.$init(form1, customRules1)
+    this.$init(form2, customRules2, 'form2')
   }
 }
 </script>
