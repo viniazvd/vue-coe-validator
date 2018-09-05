97c3255d67c985715158b8f06f11613e3f39bc6d
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-04 23:37:04 -0300

    add auto-register

--- src/support/mixin/formSetup.js
+++ src/support/mixin/formSetup.js
@@ -1,4 +1,25 @@
 const formSetup = {
+  mounted () {
+    const validation = this.$options.validation
+
+    if (validation && validation.data) {
+      const { data, rules } = validation
+
+      Object
+        .entries(data)
+        .map(([ key, value ]) => ({ key, value }))
+        .find(({ key, value }) => {
+          Object.entries(rules).find(([keyRule, valueRule]) => {
+            if (key === keyRule) {
+              this.$init(value, valueRule, key)
+            }
+          })
+        })
+    } else {
+      console.warn('follow the instructions in the documentation to correctly register the data')
+    }
+  },
+
   data () {
     return {
       forms: {},
