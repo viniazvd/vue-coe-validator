fdfc522863d17cf7b6e6398e44d7dd603292230e
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-09 19:44:53 -0300

    .

--- src/Root.vue
+++ src/Root.vue
@@ -6,17 +6,15 @@
         <c-input
           name="input1"
           label="input1"
-          :validation="$hasError('input1')"
-          :value="$getValue('input1')"
-          @input="value => $synchronize(value)" 
+          :validation="$hasError('input1', 'form1')"
+          v-model="form1.input1"
         />
 
         <c-input
           name="input2"
           label="input2"
-          :validation="$hasError('input2')"
-          :value="$getValue('input2')"
-          @input="value => $synchronize(value)"
+          :validation="$hasError('input2', 'form1')"
+          v-model="form1.input2"
         />
       </form>
 
@@ -30,8 +28,7 @@
           name="input1"
           label="input1"
           :validation="$hasError('input1', 'form2')"
-          :value="$getValue('input1', 'form2')"
-          @input="value => $synchronize(value)"
+          v-model="form2.input1"
         />
       </form>
 
@@ -54,50 +51,50 @@ export default {
 
   components: { CInput },
 
-  validation: {
-    data: {
+  data () {
+    return {
       form1: { input1: '', input2: '22' },
-      form2: { input1: '33' }
-    },
-
-    rules: {
-      form1: {
-        input1: {
-          required: true,
-          alphabetic: true
-        },
-        input2: {
-          required: true,
-          pattern: /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i
-        }
-      },
-      form2: {
-        input1: {
-          required: true,
-          alpha: true
-        }
-      }
+      form2: { input1: '33' }  
     }
   },
 
-  methods: {
-    submit1 (form) {
-      this.$allTouched(form)
-      const isValid = this.$isValidForm(form)
-
-      if (isValid) {
-        console.log('save data form1')
+  validation: {
+    form1: {
+      input1: {
+        required: true,
+        alphabetic: true
+      },
+      input2: {
+        required: true,
+        pattern: /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i
       }
     },
-    submit2 (form) {
-      this.$allTouched(form)
-      const isValid = this.$isValidForm(form)
-
-      if (isValid) {
-        console.log('save data form2')
+    form2: {
+      input1: {
+        required: true,
+        alpha: true
       }
     }
   }
+
+  // methods: {
+  //   submit1 (form) {
+  //     this.$allTouched(form)
+  //     const isValid = this.$isValidForm(form)
+
+  //     if (isValid) {
+  //       console.log('save data form1')
+  //     }
+  //   },
+  //   submit2 (form) {
+  //     this.$allTouched(form)
+  //     const isValid = this.$isValidForm(form)
+
+  //     if (isValid) {
+  //       console.log('save data form2')
+  //     }
+  //   }
+  // }
 }
 </script>
 
