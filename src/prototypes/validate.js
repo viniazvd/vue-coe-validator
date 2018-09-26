563bfceb7d8d1632770b98abf83729e72e324b9b
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-26 01:31:36 -0300

    hotfix - prevent error

--- src/prototypes/validate.js
+++ src/prototypes/validate.js
@@ -7,7 +7,7 @@ export default function (validation, messages, form, key, value) {
   let errors = []
 
   RULES.some(rule => {
-    if (validation[form][key][rule]) {
+    if (validation[form] && validation[form][key] && validation[form][key][rule]) {
       const msg = messages && messages[form][key] && messages[form][key][rule]
       const error = VALIDATIONS[rule](value, msg, validation, form, key)
       if (error) errors = [ ...errors, error ]
