6a01283286904ff137a3880f91726eafafd64a46
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-29 18:46:20 -0300

    add setListenersTouch

--- src/prototypes/index.js
+++ src/prototypes/index.js
@@ -1,3 +1,4 @@
 export { default as init } from './init'
 export { default as touch } from './touch'
 export { default as validate } from './validate'
+export { default as setListenersTouch } from './setListenersTouch'
