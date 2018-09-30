b0a8bc8e9011f72e0572a5355e9de1a7de5a43da
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-30 15:51:17 -0300

    remove

--- src/prototypes/isValidForm.js
+++
@@ -1,5 +0,0 @@
-// export default function (form = defaultForm(this.validations, defaultFormName)) {
-//   const isValid = Object.entries(this.validations[form]).every(([key, { isValid }]) => isValid)
-
-//   return isValid
-// }
