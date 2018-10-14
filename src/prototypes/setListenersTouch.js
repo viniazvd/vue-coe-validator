3f959af09c5147f49ae2d4769606145e0c1beda8
Vinicius Azevedo <viniazvd@gmail.com>
2018-10-13 22:06:55 -0300

    refactor #1

--- src/prototypes/setListenersTouch.js
+++ src/prototypes/setListenersTouch.js
@@ -1,6 +1,5 @@
-import Vue from 'vue'
-
-export default function (validations, messages) {
+export default function (validations) {
+  // dynamically records listeners to activate touch inputs
   const forms = this.$el.querySelectorAll('form[id]')
 
   if (forms.length) {
@@ -8,17 +7,7 @@ export default function (validations, messages) {
       Array.from(form.elements).forEach((element, index) => {
         // register events only for those who have validation
         if (validations[form.id][form[index].name]) {
-          form[index].addEventListener('blur', () => {
-            validations = {
-              ...validations,
-              ...this.$validator.touch(validations, messages, form.id, element.name, element.value)
-            }
-
-            // solition by @vjoao
-            Vue.util.defineReactive(validations, 'validation', validations)
-
-            this.$data.validations[form.id] = validations[form.id]
-          }, { once: true, capture: false, passive: false })
+          form[index].addEventListener('blur', () => this.$handlerBlur(form.id, element), { once: true })
         }
       })
     })
