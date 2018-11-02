bd54ed720e8c629a21d29adaa763630c646041e7
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-02 11:29:21 -0300

    defensive code

--- src/prototypes/setListenersTouch.js
+++ src/prototypes/setListenersTouch.js
@@ -1,17 +1,19 @@
 export default function (validations) {
   // dynamically records listeners to activate touch inputs
-  const forms = this.$el.querySelectorAll('form[id]')
+  this.$nextTick(() => {
+    const forms = this.$el.querySelectorAll('form[id]')
 
-  if (forms.length) {
-    forms.forEach(form => {
-      Array.from(form.elements).forEach((element, index) => {
-        // register events only for those who have validation
-        if (validations[form.id][form[index].name]) {
-          form[index].addEventListener('blur', () => this.$handlerBlur(form.id, element), { once: true })
-        }
+    if (forms.length) {
+      forms.forEach(form => {
+        Array.from(form.elements).forEach((element, index) => {
+          // register events only for those who have validation
+          if (validations[form.id][form[index].name]) {
+            form[index].addEventListener('blur', () => this.$handlerBlur(form.id, element), { once: true })
+          }
+        })
       })
-    })
-  } else {
-    console.warn('follow the instructions in the documentation to correctly register the form')
-  }
+    } else {
+      console.warn('follow the instructions in the documentation to correctly register the form')
+    }
+  })
 }
