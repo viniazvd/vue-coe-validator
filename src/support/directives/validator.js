062d00c9e753ca9662bda96ccd173aad89a06e5c
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-11-06 17:46:05 -0200

    init

---
+++ src/support/directives/validator.js
@@ -0,0 +1,34 @@
+export default {
+  validator: {
+    bind (el, binding, vnode) {
+      const [ form ] = vnode.data.model.expression.split('.')
+
+      // if the form property does not exist in validations, set.
+      if (!vnode.context.validations[form]) {
+        vnode.context.$set.call(vnode, vnode.context.validations, form, {})
+      }
+    },
+
+    inserted (el, { value: rules }, vnode) {
+      const [ form, key ] = vnode.data.model.expression.split('.')
+      const data = vnode.context[form]
+
+      // nextTick? because data was not built yet.
+      vnode.context.$nextTick(() => {
+        const validations = {
+          ...vnode.context.validations,
+          [form]: {
+            ...vnode.context.validations[form],
+            [key]: {
+              ...vnode.context.validations[form][key],
+              ...rules
+            }
+          }
+        }
+
+        vnode.context.validations = vnode.context.$validator.init(data, null, validations)
+        vnode.context.$validator.setValidations.call(vnode.context, vnode.context.validations)
+      })
+    }
+  }
+}
