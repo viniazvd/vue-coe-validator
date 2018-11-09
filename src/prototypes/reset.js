2d2a4345c6b1aeb2860122ee8004cfddd4cd102c
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-11-09 17:00:29 -0200

    refactor #1

--- src/prototypes/reset.js
+++ src/prototypes/reset.js
@@ -1,22 +1,29 @@
 export default function (formToReset) {
-  const defaultStates = ['errors', 'isDirty', 'isFilled', 'isTouched', 'isValid']
+  const defaultStates = ['isDirty', 'isFilled', 'isTouched', 'isValid']
 
-  const instanceID = Object.keys(this.context.components)[Object.keys(this.context.components).length - 1]
-  const context = this.context.components[instanceID]
+  const componentID = Object.keys(this.context.components)[Object.keys(this.context.components).length - 1]
+  const vm = this.context.components[componentID]
 
-  if (formToReset && !Object.keys(context.validations).includes(formToReset)) {
+  if (formToReset && !Object.keys(vm.validations).includes(formToReset)) {
     console.warn('it was not possible to reset the validations of a form that does not exist.')
     return
   }
-  if (!formToReset) formToReset = Object.keys(context.validations)[0]
+  if (!formToReset) formToReset = Object.keys(vm.validations)[0]
 
-  context.validations = Object
-    .entries(context.validations)
+  function setValue (state, value) {
+    if (defaultStates.includes(state)) return false
+    if (state === 'errors') return []
+
+    return value
+  }
+
+  vm.validations = Object
+    .entries(vm.validations)
     .filter(([formName]) => formName === formToReset)
     .reduce((acc, [form, fields]) => {
       acc[form] = Object.entries(fields).reduce((accFields, [key, states]) => {
         accFields[key] = Object.entries(states).reduce((accStates, [state, value]) => {
-          accStates[state] = defaultStates.includes(state) ? false : value
+          accStates[state] = setValue(state, value)
 
           return accStates
         }, {})
@@ -27,5 +34,5 @@ export default function (formToReset) {
       return acc
     }, {})
 
-  context.$validator.validateOnBlur && context.$validator.setListenersTouch.call(context, context.validations)
+  vm.$validator.validateOnBlur && vm.$validator.setListenersTouch.call(vm, vm.validations)
 }
