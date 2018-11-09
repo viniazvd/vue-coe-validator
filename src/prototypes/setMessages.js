5a74474fa6b2be23268942f1fcb36e37c73637c7
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-09 00:33:45 -0200

    init

---
+++ src/prototypes/setMessages.js
@@ -0,0 +1,11 @@
+export default (source, newMessages) => {
+  for (let form of Object.values(source)) {
+    for (let input of Object.values(form)) {
+      for (let messageKey of Object.keys(input)) {
+        if (newMessages[messageKey]) {
+          input[messageKey] = newMessages[messageKey]
+        }
+      }
+    }
+  }
+}
