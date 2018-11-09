b8481de30e3adf1835471a605a4603600660cdeb
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-11-09 18:21:06 -0200

    add initialForm(init) and setListenersTouch

--- src/support/services/index.js
+++ src/support/services/index.js
@@ -25,3 +25,65 @@ export function setSnapshot () {
     [this._uid]: getSnapshots.call(this)
   }
 }
+
+export function initialForm (data, form, validation) {
+  if (!form) form = Object.keys(validation)
+
+  const defaultState = {
+    isTouched: false,
+    isDirty: false,
+    isFilled: false,
+    isValid: false,
+    errors: []
+  }
+
+  const createForm = formName => Object.entries(data).reduce((initialForm, [key, value]) => {
+    const filled = { isFilled: !!value }
+    const dirted = { isDirty: !!value }
+    const validations = (validation && validation[key]) || (validation && validation[formName] && validation[formName][key])
+
+    initialForm[key] = { ...defaultState, ...dirted, ...filled, ...validations }
+
+    return initialForm
+  }, {})
+
+  // initialize by directive
+  if (Array.isArray(form)) {
+    let initialForm = {}
+
+    form.forEach(formName => {
+      initialForm = {
+        ...initialForm,
+        [formName]: createForm(formName)
+      }
+    })
+
+    return initialForm
+  }
+
+  // initialized by library configuration object
+  return { [form]: createForm({}) }
+}
+
+export function setListenersTouch () {
+  const componentID = Object.keys(this.$validator.context.components)[Object.keys(this.$validator.context.components).length - 1]
+  const vm = this.$validator.context.components[componentID]
+
+  // dynamically records listeners to activate touch inputs
+  vm.$nextTick(() => {
+    const forms = vm.$el.querySelectorAll('form[id]')
+
+    if (forms.length) {
+      forms.forEach(form => {
+        Array.from(form.elements).forEach((element, index) => {
+          // register events only for those who have validation
+          if (vm.validations[form.id][form[index].name]) {
+            form[index].addEventListener('blur', () => vm.$handlerBlur(form.id, element), { once: true })
+          }
+        })
+      })
+    } else {
+      console.warn('follow the instructions in the documentation to correctly register the form')
+    }
+  })
+}
