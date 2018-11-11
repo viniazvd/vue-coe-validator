f4183749911d98a05bd773c97a84f37589efcf49
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-11 00:51:35 -0200

    refactor #1

--- src/prototypes/validate.js
+++ src/prototypes/validate.js
@@ -1,41 +1,24 @@
-import RULES from '../rules/types'
-import * as VALIDATIONS from '../rules'
+import { getContext, validateField } from '../support/services'
 
-export default function (validation, messages, form, key, value) {
-  const componentID = Object.keys(this.context.components)[Object.keys(this.context.components).length - 1]
-  const vm = this.context.components[componentID]
+export default function (form) {
+  const vm = getContext.call(this)
 
-  if (!form) { console.warn('select a form to validate the data.') }
+  if (!form) form = Object.keys(vm.validations)[0]
 
-  let errors = []
+  return new Promise(resolve => {
+    let errors = []
 
-  RULES.some(rule => {
-    if (validation[form] && validation[form][key] && validation[form][key][rule]) {
-      const msg = messages && messages[form][key] && messages[form][key][rule]
-      const error = VALIDATIONS[rule](value, msg, validation, form, key)
-      if (error) errors = [ ...errors, error ]
-    }
+    Object.entries(vm.validations)
+      .filter(([formName]) => formName === form)
+      .forEach(([_, states]) => {
+        for (const key of Object.keys(states)) {
+          validateField.call(vm, form, key, vm[form][key])
+          errors.push(vm.validations[form][key].isValid)
+        }
+      })
+
+    const isFormValid = errors.every(error => error)
+
+    resolve(isFormValid)
   })
-
-  const isTouched = vm.validations[form] && vm.validations[form][key] && vm.validations[form][key].isTouched
-
-  const changed = {
-    ...validation[form][key],
-    errors,
-    isTouched: true,
-    isDirty: !!value || isTouched,
-    isFilled: !!value,
-    isValid: errors.length <= 0
-  }
-
-  const inputUpdated = { [key]: changed }
-  const formToUpdate = validation[form]
-
-  return {
-    ...validation,
-    [form]: {
-      ...formToUpdate,
-      ...inputUpdated
-    }
-  }
 }
