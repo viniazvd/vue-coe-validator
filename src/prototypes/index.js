69a04f38b84f4e6580f47b08925f6bdb9aa90ac0
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-11 00:50:57 -0200

    remove validateAll

--- src/prototypes/index.js
+++ src/prototypes/index.js
@@ -1,6 +1,5 @@
 export { default as context } from './context'
 export { default as reset } from './reset'
 export { default as validate } from './validate'
-export { default as validateAll } from './validateAll'
 export { default as init } from './init'
 export { default as snapshots } from './snapshots'
