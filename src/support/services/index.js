e12d7dba2984afc80d3f09ba44a298691dad41ec
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-11 11:19:53 -0200

    refactor #1

--- src/support/services/index.js
+++ src/support/services/index.js
@@ -1,32 +1,5 @@
-import * as VALIDATIONS from '../../rules'
-
-export function getSnapshots () {
-  const validation = this.$options.validation
-
-  /* eslint-disable */
-  const { validations = {}, messages = {}, ...data } = this.$data
-  /* eslint-enable */
-
-  return Object.entries(data).reduce((acc, [dataKey, dataValue]) => {
-    Object.keys(validation).forEach(validationKey => (validationKey === dataKey) && (acc[dataKey] = dataValue))
-
-    return acc
-  }, {})
-}
-
-export function setContext () {
-  this.$validator.context.components = {
-    ...this.$validator.context.components,
-    [this._uid]: this
-  }
-}
-
-export function setSnapshot () {
-  this.$validator.snapshots.components = {
-    ...this.$validator.snapshots.components,
-    [this._uid]: getSnapshots.call(this)
-  }
-}
+import { getContext } from '../services/context'
+import { validateField } from '../services/validate'
 
 export function setMessages (source, newMessages) {
   for (let form of Object.values(source)) {
@@ -38,79 +11,6 @@ export function setMessages (source, newMessages) {
       }
     }
   }
-}
-
-export function getContext () {
-  const vm = this.$validator || this
-  const componentID = Object.keys(vm.context.components)[Object.keys(vm.context.components).length - 1]
-
-  return vm.context.components[componentID]
-}
-
-export function getValidation (validation, form) {
-  return !validation
-    ? this.$options.validation
-    : {
-      ...this.validations,
-      [form]: {
-        ...this.validations[form],
-        ...validation
-      }
-    }
-}
-
-const defaultState = {
-  isTouched: false,
-  isDirty: false,
-  isFilled: false,
-  isValid: false,
-  errors: []
-}
-
-function setFormStates (data, form, validation) {
-  return {
-    [form]: Object.entries(data).reduce((initialForm, [key, value]) => {
-      const filled = { isFilled: !!value }
-      const dirted = { isDirty: !!value }
-      const validations = validation && validation[key]
-
-      if (validations !== undefined) {
-        initialForm[key] = { ...defaultState, ...dirted, ...filled, ...validations }
-      }
-
-      return initialForm
-    }, {})
-  }
-}
-
-export function makeInitialForm (validation, formKey, formValue) {
-  if (Object.keys(validation).includes(formKey)) {
-    this.validations = {
-      ...this.validations,
-      ...setFormStates(
-        formValue,
-        formKey, (
-          validation[formKey] || // when validation is created automatically
-          validation // when validation is created dynamically
-        )
-      )
-    }
-  }
-}
-
-function watchValidate (formKey, input) {
-  this.$watch(formKey.concat('.', input), value => {
-    validate.call(this, formKey, input, value)
-  })
-}
-
-export function setValidations (validation, form, formKey, formValue) {
-  Object.keys(validation).forEach(validationKey => {
-    if ((form && form === formKey) || validationKey === formKey) {
-      // set validator for each input
-      for (const input in formValue) watchValidate.call(this, formKey, input)
-    }
-  })
 }
 
 export function setProxy () {
@@ -136,7 +36,7 @@ function forceValidation (form, element, key = element.name, value = element.val
   if (vm.validations && !isTouched) {
     vm.validations[form][element.name].isTouched = true
 
-    validate.call(this, form, key, value || '')
+    validateField.call(this, form, key, value || '')
   }
 }
 
@@ -167,65 +67,3 @@ export function setListenersTouch () {
     }
   })
 }
-
-const defaultStates = ['isDirty', 'isFilled', 'isTouched', 'isValid']
-
-function setValue (state, value) {
-  if (defaultStates.includes(state)) return false
-  if (state === 'errors') return []
-
-  return value
-}
-
-export function resetForm (fields) {
-  return Object.entries(fields).reduce((accFields, [key, states]) => {
-    accFields[key] = Object.entries(states).reduce((accStates, [state, value]) => {
-      accStates[state] = setValue(state, value)
-
-      return accStates
-    }, {})
-
-    return accFields
-  }, {})
-}
-
-function getErrors (validations, messages, form, key, value) {
-  const hasRule = rule => validations[form] && validations[form][key] && validations[form][key][rule]
-  const getMessage = rule => messages && messages[form][key] && messages[form][key][rule]
-  const getError = (rule, msg) => VALIDATIONS[rule](value, msg, validations, form, key)
-
-  const RULES = Object.keys(VALIDATIONS)
-  let errors = []
-
-  RULES.some(rule => {
-    if (hasRule(rule)) {
-      const msg = getMessage(rule)
-      const error = getError(rule, msg)
-
-      if (error) errors = [ ...errors, error ]
-    }
-  })
-
-  return errors
-}
-
-function validate (form, key, value) {
-  const vm = getContext.call(this)
-
-  const validations = vm.validations
-  const messages = vm.messages
-
-  const errors = getErrors(validations, messages, form, key, value)
-  const isTouched = vm.validations[form] && vm.validations[form][key] && vm.validations[form][key].isTouched
-
-  vm.validations[form][key] = {
-    ...validations[form][key],
-    errors,
-    isTouched: true,
-    isDirty: !!value || isTouched,
-    isFilled: !!value,
-    isValid: errors.length <= 0
-  }
-}
-
-export const validateField = validate
