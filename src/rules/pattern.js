2a8b50ce4d6b394d4f2b30e6709342f4a47b970b
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-11 10:49:34 -0200

    fix

--- src/rules/pattern.js
+++ src/rules/pattern.js
@@ -1,5 +1,5 @@
 export default function (value, msg, forms, form, key) {
   const pattern = forms[form][key].pattern
 
-  return !pattern.test(value) && (msg || 'Campo inválido')
+  return value && !pattern.test(value) && (msg || 'Campo inválido')
 }
