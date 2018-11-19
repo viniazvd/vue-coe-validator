ed45760213e5b2fce2c6b6bb5ac7c61f61cabeb8
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-19 13:32:37 -0200

    refactor #1

--- src/prototypes/init.js
+++ src/prototypes/init.js
@@ -1,25 +1,20 @@
-import { setProxy } from '../support/services'
+import { getData, setProxy } from '../support/services'
 import { getContext } from '../support/services/context'
-import { makeInitialForm, setValidations } from '../support/services/init'
+import { setFormStates, setValidations } from '../support/services/init'
 
 function init () {
   const vm = getContext.call(this)
-
+  const data = getData.call(vm)
   const validation = vm.$options.validation
 
-  /* eslint-disable */
-  const { validations = {}, messages = {}, ...data } = vm.$data
-  /* eslint-enable */
-
-  // prevents unnecessary resources/loops
   const dataWithValidation = ([key]) => !!(validation[key] && Object.keys(validation[key]).length)
 
   Object
     .entries(data)
     .filter(dataWithValidation)
-    .forEach(([formKey, formValue]) => {
-      makeInitialForm.call(vm, validation, formKey, formValue)
-      setValidations.call(vm, validation, formKey, formValue)
+    .forEach(([form, data]) => {
+      setFormStates.call(vm, form, data, validation[form])
+      setValidations.call(vm, form, validation[form])
     })
 
   setProxy.call(vm)
