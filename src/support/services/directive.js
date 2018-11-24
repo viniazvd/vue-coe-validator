1701ce09ed59c1df995962cd77a7abf2a28f90ab
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-24 12:36:46 -0200

    init

---
+++ src/support/services/directive.js
@@ -0,0 +1,26 @@
+const isCustomInput = vnode => vnode.data.model
+
+const model = vnode => vnode.data.directives.reduce((acc, item) => {
+  if (item.name === 'model') acc[item.name] = item
+
+  return acc
+}, {}).model
+
+export function getFormValues (vnode, el) {
+  let form, key, value, inputElement
+
+  // check native or custom input
+  if (isCustomInput(vnode)) {
+    [ form, key ] = vnode.data.model.expression.split('.')
+    value = vnode.data.model.value
+    inputElement = el && el.querySelector('input')
+  } else {
+    [ form, key ] = model(vnode).expression.split('.')
+    value = model(vnode).value
+    inputElement = el
+  }
+
+  return {
+    form, key, value, inputElement
+  }
+}
