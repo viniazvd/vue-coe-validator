a15ae9c047e191f2ccb40e92b727969965de690e
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-25 17:58:50 -0200

    add isLoading, isChanged, and increment the getAsyncErrors function

--- src/support/services/validate.js
+++ src/support/services/validate.js
@@ -1,5 +1,5 @@
 import { getContext } from '../services/context'
-import { getErrors } from './'
+import { getSyncErrors, getAsyncErrors } from './'
 
 function validate (form, key, value) {
   const vm = getContext.call(this)
@@ -7,16 +7,38 @@ function validate (form, key, value) {
   const validations = vm.validations
   const messages = vm.messages
 
-  const errors = getErrors(validations, messages, form, key, value)
-  const isTouched = validations[form] && validations[form][key] && validations[form][key].isTouched
+  const state = validations[form][key]
 
-  validations[form][key] = {
-    ...validations[form][key],
-    errors,
-    isTouched: true,
-    isDirty: !!value || isTouched,
-    isFilled: !!value,
-    isValid: errors.length <= 0
+  state.isLoading = true
+
+  const hasCustomAsyncRule = state['customAsync']
+
+  const syncErrors = getSyncErrors(validations, messages, form, key, value)
+  const isTouched = validations[form] && state && state.isTouched
+
+  state.errors = syncErrors
+  state.isLoading = (hasCustomAsyncRule && !!hasCustomAsyncRule.length) || false
+  state.isChanged = true
+  state.isTouched = true
+  state.isDirty = !!value || isTouched
+  state.isFilled = !!value
+  state.isValid = syncErrors.length <= 0
+
+  if (hasCustomAsyncRule && value) {
+    getAsyncErrors(validations, messages, form, key, value)
+      .then(asyncErrors => {
+        state.errors = [...syncErrors, asyncErrors]
+        state.isLoading = false
+        state.isValid = [...syncErrors, asyncErrors].length <= 0
+      })
+  }
+
+  // scenario:
+  // -when the field has asynchronous validation
+  // -field has 1 character typed and user erases (field is empty)
+  if (hasCustomAsyncRule && !value) {
+    state.isLoading = false
+    state.isFilled = false
   }
 }
 
