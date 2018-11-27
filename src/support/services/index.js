ff567b1bab76b08ef878eefbdfe6bf12e9437163
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-26 22:22:12 -0200

    refactor #1

--- src/support/services/index.js
+++ src/support/services/index.js
@@ -1,5 +1,4 @@
 import { getContext } from '../services/context'
-import * as VALIDATIONS from '../../rules'
 
 export function setMessages (source, newMessages) {
   for (let form of Object.values(source)) {
@@ -111,59 +110,4 @@ export function setListeners () {
       console.warn('follow the instructions in the documentation to correctly register the form')
     }
   })
-}
-
-const RULES = Object.keys(VALIDATIONS)
-
-function isRule (rule, validations, form, key) {
-  return validations[form] && validations[form][key] && validations[form][key][rule]
-}
-
-function getMessage (rule, messages, form, key) {
-  return messages && messages[form] && messages[form][key] && messages[form][key][rule]
-}
-
-function getError (rule, value, msg, field) {
-  return VALIDATIONS[rule](value, msg, field)
-}
-
-export function getSyncErrors (validations, messages, form, key, value) {
-  // TO-DO/issue: change to an immutable approach?
-  let errors = []
-
-  RULES.some(rule => {
-    if (isRule(rule, validations, form, key) && rule !== 'customAsync') {
-      const msg = getMessage(rule, messages, form, key)
-      const error = getError(rule, value, msg, validations[form][key])
-
-      if (error) errors = [ ...errors, error ]
-    }
-  })
-
-  return errors
-}
-
-export async function getAsyncErrors (validations, messages, form, key, value) {
-  const msg = getMessage('customAsync', messages, form, key)
-  const error = await getError('customAsync', value, msg, validations[form][key])
-
-  return error
-}
-
-export function hasForm (form) {
-  const vm = getContext.call(this)
-
-  const isDevEnv = process.env.NODE_ENV === 'development'
-  const hasForm = Object.keys(vm.validations).includes(form)
-
-  return hasForm && isDevEnv
-}
-
-export function hasField (form, field) {
-  const vm = getContext.call(this)
-
-  const isDevEnv = process.env.NODE_ENV === 'development'
-  const hasForm = Object.keys(vm.validations[form]).includes(field)
-
-  return hasForm && isDevEnv
 }
